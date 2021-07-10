de433b7709abbbab9ac933d238ce1f9ff679a570
Rafael Martinez Bermudez <rafaelmartinez2699@gmail.com>
2021-07-10 18:20:41 -0500

    Use Redux in DevicesPage

--- app/src/views/pages/DevicesPage/DevicesPage.jsx
+++ app/src/views/pages/DevicesPage/DevicesPage.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import StyledDevicesPage from './DevicesPage.styles';
 import Card from '@components/cards/Card';
 import Wrapper from '@components/layout/wrapper/Wrapper';
 import Header from '@components/layout/header/Header';
-import data from '@/utils/data.json';
 import json2array from '@/utils/json2array';
-const devices = json2array(data.devices);
 
 const DevicesPage = () => {
+  const devices = useSelector((state) => {
+    return json2array(state.devices);
+  });
   return (
     <StyledDevicesPage>
       <Wrapper>

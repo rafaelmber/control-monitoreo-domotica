06cbbfac592df7bd1eba1d71ed8b2eca21fb2268
Rafael Martinez Bermudez <rafaelmartinez2699@gmail.com>
2021-09-03 09:46:43 -0500

    Setting firebase

--- app/src/services/firebase.js
+++ app/src/services/firebase.js
@@ -15,11 +15,18 @@ var config = {
 import firebase from 'firebase/app';
 import 'firebase/database';
 
+//databaseURL: `http://localhost:9000/?ns=control-domotica-test-default-rtdb`,
 firebase.initializeApp({
-  databaseURL: `http://localhost:9000/?ns=control-domotica-test-default-rtdb
-`,
+  apiKey: 'AIzaSyDDFU31vvO5tUEe4FRzEZ6NmgLEhWgpLpE',
+  authDomain: 'control-domotica-test.firebaseapp.com',
+  databaseURL: 'https://control-domotica-test-default-rtdb.firebaseio.com',
+  projectId: 'control-domotica-test',
+  storageBucket: 'control-domotica-test.appspot.com',
+  messagingSenderId: '553624393321',
+  appId: '1:553624393321:web:ba03d396caf78a408b74f8',
+  measurementId: 'G-BFY3J1Z9S1',
 });
 const db = firebase.database();
-db.useEmulator('localhost', 9000);
+//db.useEmulator('localhost', 9000);
 
 export default db;

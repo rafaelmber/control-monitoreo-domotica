7520a426da4e3eb2dce49821820fecd87e638289
Rafael Martinez Bermudez <rafaelmartinez2699@gmail.com>
2021-09-06 12:43:00 -0500

    Commenting App.js file

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,24 @@
+/* This is the entry point of the application 
+This is where all parts of the app are connected
+The store is initializated with Redux and also the Routing using React-Router
+
+Este es el punto de entrada de la aplicación 
+es donde se conectan las demás partes de la aplicación 
+Se inicializa el store con Redux y se iniciaiza el ruteo usando React router
+*/
+//Se importan todas las dependencias necesarias para este archivo
 import React from 'react';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { Router, Route, Switch } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 
-import GlobalStyle from './globalStyle';
-import MainApp from '@views/mainApp/MainApp';
-import reducer from './store/rootReducer';
-const store = createStore(reducer);
-const history = createBrowserHistory();
+// Se importan otros archivos dentro del proyecto para usarlos dentro de este
+import GlobalStyle from './globalStyle'; //Contiene los estilos globales de la aplicación
+import MainApp from '@views/mainApp/MainApp'; //Contiene la pantalla principal
+import reducer from './store/rootReducer'; //Es donde se define el store de Redux
+const store = createStore(reducer); // Se inicializa el store de Redux
+const history = createBrowserHistory(); //Se usa el objeto que monitorea el historial dentro dela app
 
 const App = () => {
   return (

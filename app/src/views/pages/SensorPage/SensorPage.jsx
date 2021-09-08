98150aa94ad2ec542d81c8e5ad4a367d016a8430
Rafael Martinez Bermudez <rafaelmartinez2699@gmail.com>
2021-09-08 14:49:00 -0500

    Commeting SensorPage

--- app/src/views/pages/SensorPage/SensorPage.jsx
+++ app/src/views/pages/SensorPage/SensorPage.jsx
@@ -1,4 +1,6 @@
+//Dependencias
 import React from 'react';
+//Recursos
 import StyledSensorPage from './SensorPage.styles';
 import Wrapper from '@components/layout/wrapper/Wrapper';
 import Header from '@components/layout/header/Header';
@@ -8,6 +10,10 @@ const SensorPage = () => {
     <StyledSensorPage>
       <Wrapper>
         <Header className='header' text='Sensors' />
+        {/*
+          Aquí es donde se debe agregar el contenido que se debe mostrar en la
+          página de Sensores
+          */}
         <div className='content'>Hola como estás</div>
       </Wrapper>
     </StyledSensorPage>

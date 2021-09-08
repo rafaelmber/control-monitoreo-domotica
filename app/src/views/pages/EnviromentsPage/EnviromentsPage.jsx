63d106940597a1d1e0719e5c2b663b4ac0ad9998
Rafael Martinez Bermudez <rafaelmartinez2699@gmail.com>
2021-09-08 14:57:51 -0500

    Commenting EnviromentPage

--- app/src/views/pages/EnviromentsPage/EnviromentsPage.jsx
+++ app/src/views/pages/EnviromentsPage/EnviromentsPage.jsx
@@ -1,14 +1,22 @@
+//Dependencias
 import React from 'react';
+//Recursos
 import StyledEnviromentPage from './EnviromentsPage.styles';
 import Wrapper from '@components/layout/wrapper/Wrapper';
 import Header from '@components/layout/header/Header';
 import SimpleCard from '@components/cards/simpleCard/SimpleCard';
+
 const EnviromentsPage = () => {
   return (
     <StyledEnviromentPage>
       <Wrapper>
         <Header className='header' text='Enviroments'></Header>
         <div className='content'>
+          {/*
+          Cada una de las Simple Card representa un botón que realizará 
+          acciones programadas por el usuario, encenderá y apagará dispositivos 
+          dependiendo de las preferencias del usuario
+          */}
           <SimpleCard name='Salir de casa' />
           <SimpleCard name='Ver televisión' />
           <SimpleCard name='Hora de dormir' />

cb990e626cc917edfeaca8a24420c9a8eea158eb
Rafael Martinez Bermudez <rafaelmartinez2699@gmail.com>
2021-09-08 17:52:21 -0500

    Add Login to RoomPage

--- app/src/views/pages/RoomsPage/RoomPage.jsx
+++ app/src/views/pages/RoomsPage/RoomPage.jsx
@@ -1,5 +1,5 @@
 //Dependencias
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 //Recursos
@@ -8,8 +8,10 @@ import Header from '@components/layout/header/Header';
 import Card from '@components/cards/Card';
 import Wrapper from '@components/layout/wrapper/Wrapper';
 import db from '@/services/firebase';
+import Loading from '@components/layout/loading/Loading';
 
 const RoomPage = () => {
+  const [loading, setLoading] = useState(false);
   //Se leen los dispositivos por habitación del Store central
   const dispatch = useDispatch();
   const rooms = useSelector((state) => {
@@ -25,27 +27,32 @@ const RoomPage = () => {
   });
   //Se cargan las habitaciones desde Firebase y se almacenan en el Store central
   useEffect(() => {
+    setLoading(true);
     db.ref('rooms/').once('value', (snapshot) => {
       dispatch({
         type: 'GET_ROOMS',
         payload: snapshot.val(),
       });
+      setLoading(false);
     });
   }, [dispatch]);
   return (
     <StyledRoomPage>
       <Wrapper>
         <Header className='header' text='Home' />
-        <div className='content'>
-          {/*
+        {loading && <Loading />}
+        {!loading && (
+          <div className='content'>
+            {/*
           Por cada habitación se genera una tarjeta en la que se van a mostrar
           los dispotivos de dicha habitación
           */}
-          {rooms.length > 0 &&
-            rooms.map((room) => {
-              return <Card key={room.id} {...room} />;
-            })}
-        </div>
+            {rooms.length > 0 &&
+              rooms.map((room) => {
+                return <Card key={room.id} {...room} />;
+              })}
+          </div>
+        )}
       </Wrapper>
     </StyledRoomPage>
   );

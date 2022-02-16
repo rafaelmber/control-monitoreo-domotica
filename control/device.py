f236103cf05c90e210fca8499fc089a9169185bf
Rafael Martinez Bermudez <rafaelmartinez2699@gmail.com>
2022-02-16 12:44:25 -0500

    Add device class to publish status changes using Threads with MQTT

---
+++ control/device.py
@@ -0,0 +1,20 @@
+import paho.mqtt.publish as publish
+from threading import Thread
+
+class Device():
+    def __init__(self,id:str, status: bool):
+        self.id = id
+        self.status= status
+    
+    def set_status(self,new_status: bool):
+        if new_status == True:
+            bool_status = 1
+        else:
+            bool_status = 0
+        thr = Thread(target=self._send_message, args=(bool_status,))
+        thr.start()
+        self.status = new_status
+
+    def _send_message(self, status: int):
+        publish.single(f'devices/{self.id}',str(status),client_id=f'CPU-{self.id}')
+        print(f'ID: {self.id}, Status: {self.status}')

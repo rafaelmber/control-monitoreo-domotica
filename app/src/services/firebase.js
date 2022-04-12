a26fd1653c18adacda38f2dd58beb6f11666ec4b
Rafael Martinez Bermudez <rafaelmartinez2699@gmail.com>
2022-04-12 08:20:19 -0500

    Add devices rules

--- app/src/services/firebase.js
+++ app/src/services/firebase.js
@@ -14,6 +14,7 @@ var config = {
 
 import firebase from 'firebase/app';
 import 'firebase/database';
+import 'firebase/auth';
 
 //databaseURL: `http://localhost:9000/?ns=control-domotica-test-default-rtdb`,
 firebase.initializeApp({
@@ -31,9 +32,14 @@ if (process.env.NODE_ENV === 'development') {
   db.useEmulator('localhost', 9000);
 }
 
+export const auth = firebase.auth();
+if (process.env.NODE_ENV === 'development') {
+  auth.useEmulator('http://localhost:9099');
+}
+
 export const getOnce = (name, dispatch) => {
   //Se almacenan los dispositivos dentro del Store central de Redux
-  db.ref(`${name}/`).once('value', (snapshot) => {
+  db.ref(`systems/system_1/${name}/`).once('value', (snapshot) => {
     dispatch({
       type: `GET_${name.toUpperCase()}`,
       payload: snapshot.val(),
@@ -41,61 +47,80 @@ export const getOnce = (name, dispatch) => {
   });
 };
 
-export const removeEnviroment = async (id) => {
-  const environmentRef = db.ref('enviroments/' + id);
+export const removeEnviroment = async (envId) => {
+  const environmentRef = db.ref('systems/system_1/environments/' + envId);
   await environmentRef.remove();
 };
 
-export const removeDevice = async (id) => {
-  return db
-    .ref('enviroments/')
-    .once('value', (snapshot) => {
-      snapshot.forEach((envChild) => {
-        envChild
-          .child('devices')
-          .child(id)
-          .ref.remove(() => {
-            console.log(`${id} was Deleted from ${envChild.key}`);
-          });
-      });
-    })
-    .then(() => {
-      db.ref('devices/' + id).remove(() => {
-        console.log(id, 'was removed');
-      });
-    });
-};
-
 // export const removeDevice = async (id) => {
-//   const refEnviroment = db.ref('enviroments/');
-//   const snapEnviroment = await refEnviroment.once('value');
-//   for (const [key, env] of Object.entries(snapEnviroment.val())) {
-//     console.log(key, env);
-//   }
-//   const refDevice = db.ref('devices/' + id);
-//   // await refDevice.remove();
-//   console.log(id, 'was removed');
-// };
-
-// export const removeRoom = async (id) => {
-//   return db.ref('rooms/' + id + '/devices/').once('value', (snapshot) => {
-//     snapshot.forEach((deviceByRoom) => {
-//       removeDevice(deviceByRoom.key).then(() => {
-//         db.ref('rooms/' + id).remove(() => {
-//           console.log(id, 'was deleted');
-//         });
+//   return db
+//     .ref('system/system_1/environments/')
+//     .once('value', (snapshot) => {
+//       snapshot.forEach((envChild) => {
+//         envChild
+//           .child('devices')
+//           .child(id)
+//           .ref.remove(() => {
+//             console.log(`${id} was Deleted from ${envChild.key}`);
+//           });
+//       });
+//     }).then(()=>{
+//       db.ref('systems/system_1/rooms').once
+//     })
+//     .then(() => {
+//       db.ref('system/system_1/devices/' + id).remove(() => {
+//         console.log(id, 'was removed');
 //       });
 //     });
-//   });
 // };
 
+export const removeDevice = async (deviceId) => {
+  await removeDeviceFromEnvs(deviceId);
+  await removeDeviceFromRooms(deviceId);
+  const deviceRef = db.ref('systems/system_1/devices/' + deviceId);
+  await deviceRef.remove();
+  console.log(`${deviceId} Removed`);
+};
+
+const removeDeviceFromEnvs = async (deviceId) => {
+  //Delete device from Environments
+  const envsRef = db.ref('systems/system_1/environments/');
+  const snapshotEnv = await envsRef.once('value');
+  snapshotEnv.forEach((envChild) => {
+    if (envChild.exists()) {
+      const device = envChild.child('devices').child(deviceId);
+      if (device.exists()) {
+        device.ref.remove(() => {
+          console.log(`${deviceId} was Deleted from ${envChild.key}`);
+        });
+      }
+    }
+  });
+};
+
+const removeDeviceFromRooms = async (deviceId) => {
+  //Delete device from Rooms
+  const roomsRef = db.ref('systems/system_1/rooms/');
+  const snapshotRoom = await roomsRef.once('value');
+  snapshotRoom.forEach((roomChild) => {
+    if (roomChild.exists()) {
+      const device = roomChild.child('devices').child(deviceId);
+      if (device.exists()) {
+        device.ref.remove(() => {
+          console.log(`${deviceId} Removed from ${roomChild.key}`);
+        });
+      }
+    }
+  });
+};
+
 export const removeRoom = async (id) => {
-  const deviceRef = db.ref('rooms/' + id + '/devices/');
+  const deviceRef = db.ref('systems/system_1/rooms/' + id + '/devices/');
   const deviceSnap = await deviceRef.once('value');
   for (const device in deviceSnap.val()) {
     await removeDevice(device);
   }
-  const roomRef = db.ref('rooms/' + id);
+  const roomRef = db.ref('systems/system_1/rooms/' + id);
   await roomRef.remove();
   console.log(id, 'room was removed');
 };
